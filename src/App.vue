edb99a946f0fd602a089117a725541a8b8cefdfe
myeonghan-nim <ataraxia9408@gmail.com>
2021-12-20 01:45:39 +0900

    Fix: ESLint

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <template>
   <div>
-
-    <h1>Root!</h1>
-
-    <!-- 3. use loaded component -->
-    <todo-list category="Career"></todo-list>
-    <todo-list category="SSAFY"></todo-list>
+    <h1>MAIN</h1>
     <TodoList category="Shopping"/>
-
   </div>
 </template>
 
 <script>
-// 1. load component
 import TodoList from './components/TodoList.vue'
 
 export default {
-  // 2. add component
   components: {
     TodoList,
   }
@@ -24,5 +16,4 @@ export default {
 </script>
 
 <style>
-
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,30 +1,24 @@
 <template>
   <div class="todo-list">
-
     <h1>{{category}}</h1>
-
     <input type="text" v-model="newTodo" v-on:keyup.enter="addTodo">
     <button v-on:click="addTodo">+</button>
-
     <li v-for="todo in todos" v-bind:key="todo.id">
       <span>{{todo.content}}</span>
       <button v-on:click="removeTodo(todo.id)">x</button>
     </li>
-
   </div>
 </template>
 
 <script>
 export default {
   props: ['category'],
-
   data: function() {
     return {
       todos: [],
       newTodo: ''
     }
   },
-
   methods: {
     addTodo: function(e) {
       if (this.newTodo.length != 0) {
@@ -36,7 +30,6 @@ export default {
         this.newTodo = ''
       }
     },
-
     removeTodo: function(todoID) {
       this.todos = this.todos.filter(todo => {
         return todo.id !== todoID
